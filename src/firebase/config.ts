bb7ec77f8bc9bef1b87360b078dc97080e6de9ff
kollabathula ratan <kollabathularatan@gmail.com>
2026-03-20 05:30:27 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3558200235-6f3e2",
+  "appId": "1:793366210674:web:7865b1ed2a71cd91e47aeb",
+  "apiKey": "AIzaSyDsKqNIhQKUuQ_5UayIxGrsPibKIesd6Pw",
+  "authDomain": "studio-3558200235-6f3e2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "793366210674"
+};
